d7445a7abc81278034b57c2f5f1b9c1d28ba7650
Bu4y <amnes1a@windowslive.com>
2018-02-06 10:25:22 +0700

    shippingmaster

--- modules/shippingmasters/server/routes/shippingmasters.server.routes.js
+++ modules/shippingmasters/server/routes/shippingmasters.server.routes.js
@@ -4,15 +4,16 @@
  * Module dependencies
  */
 var shippingmastersPolicy = require('../policies/shippingmasters.server.policy'),
+  core = require('../../../core/server/controllers/core.server.controller'),
   shippingmasters = require('../controllers/shippingmasters.server.controller');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Shippingmasters Routes
-  app.route('/api/shippingmasters').all(shippingmastersPolicy.isAllowed)
+  app.route('/api/shippingmasters').all(core.jwtCheck, shippingmastersPolicy.isAllowed)
     .get(shippingmasters.list)
     .post(shippingmasters.create);
 
-  app.route('/api/shippingmasters/:shippingmasterId').all(shippingmastersPolicy.isAllowed)
+  app.route('/api/shippingmasters/:shippingmasterId').all(core.jwtCheck, shippingmastersPolicy.isAllowed)
     .get(shippingmasters.read)
     .put(shippingmasters.update)
     .delete(shippingmasters.delete);
