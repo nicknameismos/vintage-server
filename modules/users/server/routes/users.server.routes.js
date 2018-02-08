a76dc0797094b71fa9c63ff22c65ef263a3dc0cf
Bu4y <amnes1a@windowslive.com>
2018-02-08 12:24:23 +0700

    update

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -2,14 +2,15 @@
 
 module.exports = function (app) {
   // User Routes
-  var users = require('../controllers/users.server.controller');
+  var users = require('../controllers/users.server.controller'),
+    core = require('../../../core/server/controllers/core.server.controller');
 
   // Setting up the users profile api
   app.route('/api/users/me').get(users.me);
   app.route('/api/users/me').post(users.me);
   app.route('/api/users').put(users.update);
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
-  app.route('/api/users/password').post(users.changePassword);
+  app.route('/api/users/password').post(core.jwtCheck, users.changePassword);
   app.route('/api/users/picture').post(users.changeProfilePicture);
   app.route('/api/usermanage/:userId').put(users.usermanage);
 
