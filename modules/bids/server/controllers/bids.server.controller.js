4eecad5840e135ee35789f10745057659dddf8f6
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-02-19 15:15:24 +0700

    update

--- modules/bids/server/controllers/bids.server.controller.js
+++ modules/bids/server/controllers/bids.server.controller.js
@@ -292,7 +292,8 @@ exports.getBidDetail = function (req, res) {
 };
 
 exports.scheduleBid = function (req, res) {
-  var startTime = new Date(req.bid.endtime + 10000).setHours(expiredate.getHours() - 7);
+  var startTime = new Date(req.bid.endtime + 10000).setHours(expiredate.getHours() + 7);
+  console.log(startTime);
   var j = schedule.scheduleJob({
     start: startTime
   }, function () {
