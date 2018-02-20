49e805667a4b19f193514a21ebcb9539281d243f
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-02-20 13:04:13 +0700

    update

--- config/lib/app.js
+++ config/lib/app.js
@@ -57,7 +57,7 @@ module.exports.start = function start(callback) {
       console.log('--');
 
       if (callback) callback(app, db, config);
-      console.log(config.utils.getGlobbedPaths());
+      console.log(process.env.PORT);
       request({
         url: serverUrl + '/api/createBidsScheduleJob',
         method: 'GET',
