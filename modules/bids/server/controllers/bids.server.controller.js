8aeb7f8976719226aa0226cab775c1d4b9c06ced
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-02-23 15:52:03 +0700

    update

--- modules/bids/server/controllers/bids.server.controller.js
+++ modules/bids/server/controllers/bids.server.controller.js
@@ -226,8 +226,8 @@ exports.cookingBid = function (req, res, next) {
 
               // cookingData[2].items = _.chain(cookingData[2].items).sortBy('dateend').sortBy('isBid').value();
               cookingData[2].items = cookingData[2].items.sort(function (a, b) {
-                var aa = a.isBid + a.dateend;
-                var bb = b.isBid + b.dateend;
+                var aa = a.dateend + a.isBid;
+                var bb = b.dateend + b.isBid;
                 return (aa < bb ? 1 : aa > bb ? -1 : 0);
               });
 
