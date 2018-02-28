9b64dd393f78c9e952ebc8adf21e0b16396a87f2
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-02-28 10:50:56 +0700

    update

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -141,7 +141,7 @@ exports.updateNotification = function (req, res) {
             var item = orderRes4.items[orderRes4.items.map(function (e) {
               return e._id.toString();
             }).indexOf(req.body.itemid.toString())];
-            
+
             var orderid = orderRes4.docno ? orderRes4.docno : orderRes4._id;
 
             if (item.status === 'cancel') {
@@ -219,7 +219,7 @@ exports.updateNotification = function (req, res) {
               item.log.forEach(function (log) {
                 if (log.status === 'sent') {
                   var date = new Date(log.created);
-                  dateStatus = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + (date.getHours() + 7) + ':' + (date.getMinutes() > 9) ? date.getMinutes() : '0' + date.getMinutes();
+                  dateStatus = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + (date.getHours() + 7) + ':' + date.getMinutes();
                 }
               });
 
